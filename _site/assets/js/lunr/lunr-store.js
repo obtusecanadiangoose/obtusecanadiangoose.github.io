d58f3b00e718ac7ffcda4a98005a935ef5971c7d
Liam <ogilvieliam@gmail.com>
2020-09-17 11:49:29 -0400

    done for now

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Learning the Ropes",
+        "excerpt":"These are some headers: H1 H2 H3 H4 H5 H6 This text is bold This text is italic How about some bold italic This is a link to Google that I can link down below is the #1 result for “cute cat” on google images This is in a code...","categories": ["general"],
+        "tags": [],
+        "url": "http://localhost:4000/general/first-post/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      }]
